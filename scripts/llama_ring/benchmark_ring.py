b707d913a0708db6f30e5a0077d12e841bd4632f
sadigulcelik <sadigulcelik@gmail.com>
2025-05-07 18:58:26 -0400

    Update benchmark_ring.py

--- scripts/llama_ring/benchmark_ring.py
+++ scripts/llama_ring/benchmark_ring.py
@@ -285,7 +285,7 @@ def main():
     if not args.run_ring_first:
         strategies.reverse()
 
-    prompt_n_values = [10, 20, 50, 100, 200, 300, 400]
+    prompt_n_values = [10, 100, 400, 500]
 
     for strategy_label, strategy in strategies:
 
